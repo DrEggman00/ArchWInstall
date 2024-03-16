d13d1be6c315c8e0a294a803c3cc699746481c73
Daniel Vinicius da Silva <101137341+DanielVinicius00@users.noreply.github.com>
2024-03-16 18:31:37 -0300

    Update install_baseKDE.sh

--- ArchInstall/install_baseKDE.sh
+++ ArchInstall/install_baseKDE.sh
@@ -4,21 +4,25 @@ pacman -Syyuu
 
 ln -sf /usr/share/zoneinfo/America/Araguaina /etc/localtime
 
-echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
+echo "pt_BR.UTF-8 UTF-8" > /etc/locale.gen
 
 locale-gen
 
-echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
+echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
 
-echo "computador" >> /etc/hostname
+echo "computador" > /etc/hostname
 
-echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
+echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
 
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=Arch --recheck
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
+echo "Senha do Root"
+
 passwd
+
+echo "Adicionando usuário padrão James"
 
 useradd -m -g users -G wheel,storage,power -s /bin/bash james
 
