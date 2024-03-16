4ea591e73da0c503b41c3de517a0a9d138fdd8bf
Daniel Vinicius da Silva <101137341+DanielVinicius00@users.noreply.github.com>
2024-03-16 18:29:46 -0300

    Update install_basic.sh

--- ArchInstall/install_basic.sh
+++ ArchInstall/install_basic.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-mkfs.vfat -F32 /dev/nvme0n1p1
-
-mkfs.ext4 /dev/nvme0n1p2
-
-echo "Montando partições"
-
-mount /dev/nvme0n1p2 /mnt
-
-mkdir /mnt/efi
-
-mount /dev/nvme0n1p1 /mnt/efi
-
 cp -rv mirrorlist /etc/pacman.d/mirrorlist
 
 pacstrap -K /mnt base base-devel linux-firmware nano linux grub efibootmgr
